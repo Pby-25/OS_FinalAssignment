ad0ac63a8ab9fcad85bfeaac338d2c65750de139
Pby-25 <agains1595@gmail.com>
2017-12-04 15:18:39 -0500

    updated gitignore

--- bitmap.c
+++ bitmap.c
@@ -4,13 +4,11 @@
 
 #include <strings.h>    // for `ffs`
 
-/* constants */
-// how far to loop in array
-#define SIZE (NUM_BLOCKS/8) //128
+
 
 /* globals */
 // the actual data. initialize all bits to high
-uint8_t free_bit_map[SIZE] = { [0 ... SIZE-1] = UINT8_MAX };
+uint8_t free_bit_map[BITMAP_ROW_SIZE] = { [0 ... BITMAP_ROW_SIZE-1] = UINT8_MAX };
 
 /* macros */
 #define FREE_BIT(_data, _which_bit) \
@@ -20,8 +18,8 @@ uint8_t free_bit_map[SIZE] = { [0 ... SIZE-1] = UINT8_MAX };
     _data = _data & ~(1 << _which_bit)
 
 void force_set_index(uint32_t index) {
-    // Used to force indicies to used 
-    // this is the opposite of rm_index. 
+    // Used to force indicies to used
+    // this is the opposite of rm_index.
     uint32_t i = index / 8;
     uint8_t bit = index % 8;
     USE_BIT(free_bit_map[i], bit);
@@ -39,7 +37,7 @@ uint32_t get_index() {
         The ffs() function returns the position of the first (least
        significant) bit set in the word i.  The least significant bit is
        position 1 and the most significant position is, for example, 32 or
-       64.  
+       64.
     */
     // Since ffs has the lsb as 1, not 0. So we need to subtract
     uint8_t bit = ffs(free_bit_map[i]) - 1;
@@ -62,4 +60,3 @@ void rm_index(uint32_t index) {
     // free bit
     FREE_BIT(free_bit_map[i], bit);
 }
-
--- bitmap.h
+++ bitmap.h
@@ -4,13 +4,16 @@
 #include <stdint.h>
 
 #define NUM_BLOCKS 1024
+#define BITMAP_ROW_SIZE (NUM_BLOCKS/8) // this essentially mimcs the number of rows we have in the bitmap. we will have 128 rows.
+
+extern uint8_t free_bit_map[BITMAP_ROW_SIZE];
 
 /*
  * @short force an index to be set.
  * @long Use this to setup your superblock, inode table and free bit map
  *       This has been left unimplemented. You should fill it out.
  *
- * @param index index to set 
+ * @param index index to set
  *
  */
 void force_set_index(uint32_t index);
@@ -28,5 +31,3 @@ uint32_t get_index();
 void rm_index(uint32_t index);
 
 #endif //_INCLUDE_BITMAP_H_
-
-
